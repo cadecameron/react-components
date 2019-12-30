8fdd143566a0a69da4ac8f5ea00f4cf7972399a4
cadecameron <cade.cameron@gmail.com>
2019-12-30 11:50:38 -0800

    Completed up to Component Properties aka props

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList list={['Eggs', 'Milk']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.list.map(element => {
+      return <GroceryListItem item={element} />
+    })}
+  </ul>
+);
+
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
